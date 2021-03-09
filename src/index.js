bc349c8f40a6023d9ceb247df124722c60a036e5
Yuliana Sadovskaya <juliana.sad07@gmail.com>
2021-03-10 01:02:25 +0300

    init: complete "reverse-int" task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let nArr = `${Math.abs(n)}`.split('');
+    let newArr = [];
+    for (let i = nArr.length - 1; i >= 0; i--){
+            newArr.push(nArr[i]);
+    }
+    return Number(newArr.join(''));
 }
